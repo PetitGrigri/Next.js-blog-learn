09ca4c1c6d361a881fe9f827a268595c223ca0e5
Griselles Fabien <fgriselles@hotmail.fr>
2021-02-13 20:49:01 +0100

    [Next-2 | Task] Remove usage of next/image to allow static export (#2)

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Link from 'next/link'
 import styles from './layout.module.sass'
 import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
- 
+
 const name = 'PetitGrigri';
 
 export const siteTitle = 'Next.js Sample Website'
@@ -29,8 +28,7 @@ export default function Layout({ children, home }) {
       <header className={styles.header}>
         {home ? (
           <>
-            <Image
-              priority
+            <img
               src="/images/profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
@@ -43,8 +41,7 @@ export default function Layout({ children, home }) {
           <>
             <Link href="/">
               <a>
-                <Image
-                  priority
+                <img
                   src="/images/profile.jpg"
                   className={utilStyles.borderCircle}
                   height={108}
